d1dae4058529d642117f98d261972d155d7cf9c4
KBieszczad <krzybies@gmail.com>
2026-04-24 15:18:26 +0200

    entities

---
+++ SalonSamochodowy/Entities/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalonSamochodowy.Entities
+{
+    public class Engine
+    {
+        public int EngineID { get; set; }
+        [MaxLength(50)]
+        public string EngineName { get; set; } = null!;
+        [MaxLength(20)]
+        public string EngineSize { get; set; } = null!;
+        public int Power { get; set; }
+        public decimal Price { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+
+    }
+}
