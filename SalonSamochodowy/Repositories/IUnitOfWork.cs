e6b6bc93758ee57ac5154fe5af9d357fff46c8b0
SKwiecinski <szymon.kwie@gmail.com>
2026-05-16 23:01:52 +0200

    feat: add interface and implementation of the Unit of Work pattern
    Commit wprowadza wzorzec Unit of Work w celu koordynowania transakcji pomiędzy wieloma repozytoriami:
    - IUnitOfWork.cs: Udostępnia właściwości dla wszystkich repozytoriów oraz metodę CompleteAsync do zatwierdzania transakcji.
    - UnitOfWork.cs: Zarządza instancją AppDbContext i z opóźnieniem inicjuje generyczne repozytoria, aby upewnić się, że wszystkie operacje w ramach danej akcji biznesowej współdzielą ten sam stan kontekstu bazy danych.
    
    Szczegółowe wyjaśnienie pod linkami w kodzie.
    Komentarze w kodzie zrobione z asystą AI.

---
+++ SalonSamochodowy/Repositories/IUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using SalonSamochodowy.Entities;
+
+namespace SalonSamochodowy.Repositories
+{
+    /**
+     * Interface for the Unit of Work pattern, managing multiple repositories and database transactions.
+     * 
+     * https://medium.com/@differentiate.function/understanding-unit-of-work-in-c-and-its-implementation-with-entity-framework-core-613552995237
+     * https://www.netmentor.es/entrada/en/unit-of-work
+     * https://learn.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application
+     */
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<AppRole> AppRoles { get; }
+        IRepository<AppUser> AppUsers { get; }
+        IRepository<Client> Clients { get; }
+        IRepository<Dealership> Dealerships { get; }
+        IRepository<Engine> Engines { get; }
+        IRepository<Feature> Features { get; }
+        IRepository<Job> Jobs { get; }
+        IRepository<SalesOrder> SalesOrders { get; }
+        IRepository<TrimFeature> TrimFeatures { get; }
+        IRepository<TrimLevel> TrimLevels { get; }
+        IRepository<Vehicle> Vehicles { get; }
+        IRepository<VehicleFeature> VehicleFeatures { get; }
+        IRepository<VehicleModel> VehicleModels { get; }
+        IRepository<Worker> Workers { get; }
+
+        /**
+         * Saves all changes made in this context to the database.
+         * * @return the number of state entries written to the database.
+         */
+        Task<int> CompleteAsync();
+    }
+}
