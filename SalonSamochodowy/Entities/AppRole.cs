083dcf0ff6763db53c78141b886100acf323c484
KBieszczad <krzybies@gmail.com>
2026-05-18 18:31:11 +0200

    fix: missing entities

---
+++ SalonSamochodowy/Entities/AppRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalonSamochodowy.Entities
+{
+    public class AppRole
+    {
+        public int RoleID { get; set; }
+        [MaxLength(50)]
+        public string RoleName { get; set; } = null!;
+        public int PermissionLevel { get; set; }
+
+        public virtual ICollection<AppUser> Users { get; set; } = new List<AppUser>();
+    }
+}
---
+++ SalonSamochodowy/Entities/TrimFeature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalonSamochodowy.Entities
+{
+    public class TrimFeature
+    {
+        public int TrimID { get; set; }
+        public virtual TrimLevel Trim { get; set; } = null!;
+        public int FeatureID { get; set; }
+        public virtual Feature Feature { get; set; } = null!;
+        public bool IsStandard { get; set; }
+        public decimal AdditionalPrice { get; set; }
+
+    }
+}
