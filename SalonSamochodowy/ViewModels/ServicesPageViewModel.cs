8268ea07d59e057ea28bbd17633a4dd9e6bebce5
Marek <marekznm@gmail.com>
2026-05-25 23:36:58 +0200

    Started working on ServicesPageViewModel

--- SalonSamochodowy/ViewModels/ServicesPageViewModel.cs
+++ SalonSamochodowy/ViewModels/ServicesPageViewModel.cs
@@ -1,5 +1,7 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using SalonSamochodowy.Entities;
+using SalonSamochodowy.Repositories;
 
 namespace SalonSamochodowy.ViewModels
 {
@@ -8,8 +10,45 @@ namespace SalonSamochodowy.ViewModels
         public ObservableCollection<ServiceJob> PendingJobs { get; } = new();
         public ObservableCollection<ServiceJob> InProgressJobs { get; } = new();
         public ObservableCollection<ServiceJob> FinishedJobs { get; } = new();
-    }
+        public async Task LoadFromDbAsync()
+        {
 
+            /*
+             zaœ tu trzeba zajebaæ trochu rozkminki
+            zebrac userID zalogowanego
+            bo kierownik musi widziec wszystkie zlecenia jego mrówek
+            a mrówka tylko swój
+             */
+
+            try
+            {
+                using var ctx = new AppDbContext();
+                using var uow = new UnitOfWork(ctx);
+
+                if(loggedInUserRole == "Kierownik")
+                {
+
+                }
+                else if(loggedInUserRole == "Serwisant")
+                {
+
+                }
+
+                    var allJobs = await uow.Jobs.GetAllAsync();
+
+                foreach (var job in allJobs)
+                {
+                    String jobStatus = job.Status;
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+    }
     public class ServiceJob
     {
         public string TaskName { get; set; } = "";
